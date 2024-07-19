efcdd0266d67f1235624e757747a22c6b7dd7ca5
khanhnqph34669 <khanhnqph34669@fpt.edu.vn>
2024-07-19 22:03:03 +0700

    CM

--- resources/views/Client/searchResults.blade.php
+++ resources/views/Client/searchResults.blade.php
@@ -11,7 +11,7 @@
                     <div class="card">
                         <img src="{{ $post->thumbnail }}" class="card-img-top" alt="{{ $post->title }}">
                         <div class="card-body">
-                            <h5 class="card-title"><a href="{{ route('detailPost', $post->id) }}">{{ $post->title }}</a></h5>
+                            <h5 class="card-title"><a href="{{ route('detailPost', $post->slug) }}">{{ $post->title }}</a></h5>
                             <p class="card-text">{{ Str::limit($post->content, 150) }}</p>
                         </div>
                     </div>
